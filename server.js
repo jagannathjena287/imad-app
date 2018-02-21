1b5adfa916d2a8034db1bc04080c30a995957044
jagannathjena287 <jagannathjena287@gmail.com>
2018-02-21 23:12:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var articleOne = {
         </p>`
 };
 
-function crreateTemplate (data) {
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res) {
-  res.send(createTemplate(articleone));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req, res) {
